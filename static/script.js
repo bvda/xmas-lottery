716ed60ddb63e8665751c346b8cf3dd924867696
Brian Danielsen <B279199@prod.sitad.dk>
2024-11-20 16:07:45 +0100

    Add stuff

--- static/script.js
+++ static/script.js
@@ -1,3 +1,33 @@
+let ticker;
+let count = 0
+
+function pull() {
+    $.ajax({
+        url: '/get_draw',
+        type: 'GET',
+        contentType: 'application/json',
+        success: (resp) => {
+            $('#current').empty()
+            let current = resp.response[1].at(-1)
+            console.log(current)
+            $('#current').append('<div class="roboto-mono-medium round ' + current.color + '">' + 
+                '<p>Litra</p>' +
+                '<p> NR.' +  String(current.number).padStart(3, '0') + '</p></div>')
+            $('#numbers').empty()
+            for(var n of resp.response[1].slice(0, -1)) {
+                $('#numbers').append('<div class="roboto-mono-medium round ' + n.color + '">' + n.number + '</div>')
+                console.log(n)
+            }
+            console.log(resp)
+            count++
+            if(count >= 5  ) {
+                clearInterval(ticker)
+                count = 0
+            }
+        }
+    })
+}
+
 $(document).ready(function() {       
      $.ajax({
             url: '/get_data',
@@ -24,25 +54,8 @@ $(document).ready(function() {
         }})
     })
     $('#sendButton').click(function() {
-        setInterval(() => {
-            $.ajax({
-                url: '/get_draw',
-                type: 'GET',
-                contentType: 'application/json',
-                success: (resp) => {
-                    $('#current').empty()
-                    let current = resp.response[1].at(-1)
-                    console.log(current)
-                    $('#current').append('<div class="roboto-mono-medium round ' + current.color + '">' + current.number + '</div>')
-                    $('#numbers').empty()
-                    for(var n of resp.response[1].slice(0, -1)) {
-                        $('#numbers').append('<div class="roboto-mono-medium round ' + n.color + '">' + n.number + '</div>')
-                        console.log(n)
-                    }
-                    console.log(resp)
-                }
-            })
-        }, 5000)
+        pull()
+        ticker = setInterval(pull, 1000)
     });
 
 });
